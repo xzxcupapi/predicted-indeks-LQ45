c63654d5f21b1418ca1faee352236b686f1fb1a2
Arjuna Satria <arjunasatria1717@gmail.com>
2024-02-06 18:33:11 +0700

    Finalize Model

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import xgboost as xgb
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, r2_score
 import joblib
 from flask import Flask, render_template, request
 
@@ -13,6 +14,11 @@ data['Date'] = pd.to_datetime(data['Date'])
 
 # Feature Engineering 
 data['Return'] = (data['Close'] - data['Open']) / data['Open']
+
+# Imputasi nilai yang hilang
+imputer = SimpleImputer(strategy='mean')  
+data[['High', 'Low', 'Open', 'Volume']] = imputer.fit_transform(data[['High', 'Low', 'Open', 'Volume']])
+
 
 # Pembagian Data
 X = data[['High', 'Low', 'Open', 'Volume']]  
@@ -33,6 +39,14 @@ model.fit(X_train_scaled, y_train)
 y_pred = model.predict(X_test_scaled)
 mse = mean_squared_error(y_test, y_pred)
 print('Mean Squared Error:', mse)
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+print("RMSE:", rmse)
+r2 = r2_score(y_test, y_pred)
+print("R^2:", r2)
+mae = mean_absolute_percentage_error(y_test, y_pred)
+print("MAE:", mae)
+mape = np.mean(np.abs((y_test - y_pred) / y_test)) * 100
+print("MAPE:", mape)
 
 # Simpan Model
 joblib.dump(model, 'xgboost_model.pkl')
@@ -43,6 +57,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+def denormalize(scaled_value, scaler):
+    min_val = scaler.data_min_[3]
+    max_val = scaler.data_max_[3]
+    return scaled_value * (max_val - min_val) + min_val
 
 @app.route('/predict', methods=['POST'])
 def predict():
